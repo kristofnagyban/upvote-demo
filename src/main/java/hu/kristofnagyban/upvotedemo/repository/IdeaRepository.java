3d1edb31ab59c601e6e3d444b6d651ee643ed0be
Kristóf Nagybán <kristof.nagyban@gmail.com>
2021-06-15 12:25:42 +0200

    IdeaRepository findByApprovedTrue and findByApprovedFalse queries added.

--- src/main/java/hu/kristofnagyban/upvotedemo/repository/IdeaRepository.java
+++ src/main/java/hu/kristofnagyban/upvotedemo/repository/IdeaRepository.java
@@ -4,6 +4,12 @@ import hu.kristofnagyban.upvotedemo.domain.Idea;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IdeaRepository extends JpaRepository<Idea, Long> {
+
+    List<Idea> findByApprovedTrue();
+
+    List<Idea> findByApprovedFalse();
 }
